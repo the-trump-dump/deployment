e72c782c184c8da635c2c58f9db7ab1d2568b910
Josh Long <josh@joshlong.com>
2021-01-10 21:26:16 -0800

    gi

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Deploying This Week In..."
+
+SECRETS_FN=twi-configmap.env
+cat <<EOF >${SECRETS_FN}
+VUE_APP_SERVICE_ROOT=https://ttd-editor-api.cfapps.io
+EOF
+
+kubectl apply -k .
+rm $SECRETS_FN
